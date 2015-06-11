25ad6b3f1089cdbc6b66abdf7d189c1f5acad27d
caiqueminhare <caique.minhare@gmail.com>
2015-06-11 16:45:07 -0400

    Update trabalho.c

--- trabalho.c
+++ trabalho.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void){
 
-    FILE *ifp, *ofp;
-    char *mode = "r";
-    char outputFilename[] = "resultado.txt";
-    int ch, y;
-    char rola[] = "rola";
+FILE *ifp, *ofp;
+char *mode = "r";
+char outputFilename[] = "resultado.txt";
+int ch, y, count, count1;
 
     ifp = fopen("arquivo.txt", mode);
 
-    if (ifp == NULL) 
-    {
+    if (ifp == NULL) {
     fprintf(stderr, "Erro ao abrir o arquivo.txt!\n");
     exit(1);
     }
 
-    ofp = fopen(outputFilename, "w");
+ofp = fopen(outputFilename, "w");
 
-    if (ofp == NULL) 
-    {
+    if (ofp == NULL) {
     fprintf(stderr, "Erro ao salvar o arquivo %s!\n", outputFilename);
     exit(1);
     }
 
-    ch = 40;
-    fputs(rola,ofp);
+    // Escaneia o arquivo ifp e salva os valores da coluna e linha //
+    fscanf(ifp,"%d %d",&ch,&y);
+    char matAux[ch+1][y];
+    for(count=0 ; count < ch ; count++)
+        for(count1=0 ; count1 < y ; count1++)
+            fscanf(ifp, " %c", &matAux[count][count1]);
+
+    for(count=0 ; count < ch ; count++){
+        for(count1=0 ; count1 < y ; count1++)
+            fprintf(ofp, "%c", matAux[count][count1]);
+        fprintf(ofp,"\n");
+    }
+
+    return 0;
+}
