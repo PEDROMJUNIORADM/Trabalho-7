5f26e97470db72d331e5079f5947692836a73f85
caiqueminhare <caique.minhare@gmail.com>
2015-06-11 15:36:57 -0400

    Update trabalho.c

--- trabalho.c
+++ trabalho.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
-lalala
+#include <stdlib.h>
+
+int main(void){
+
+FILE *ifp, *ofp;
+char *mode = "r";
+char outputFilename[] = "resultado.txt";
+int ch, y;
+char rola[] = "rola";
+
+    ifp = fopen("arquivo.txt", mode);
+
+    if (ifp == NULL) {
+    fprintf(stderr, "Erro ao abrir o arquivo.txt!\n");
+    exit(1);
+    }
+
+ofp = fopen(outputFilename, "w");
+
+    if (ofp == NULL) {
+    fprintf(stderr, "Erro ao salvar o arquivo %s!\n", outputFilename);
+    exit(1);
+    }
+
+    ch = 40;
+    fputs(rola,ofp);
