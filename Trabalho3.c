e3a162b7c9728ff435437859001a8dfcc45ee6e0
caiqueminhare <caique.minhare@gmail.com>
2015-06-18 22:45:04 -0400

    Update Trabalho3.c

--- Trabalho3.c
+++ Trabalho3.c
@@ -8,7 +8,7 @@ int getTamColuna(FILE *file);
 void makeMatriz(FILE *file, char (*matriz)[TAM][TAM], char mat[TAM][TAM], int *tamColuna, int *tamLinha, int *qntdPalavras);
 void printaMatriz( char (*matriz)[TAM][TAM], int tamLinha, int tamColuna );
 void putMatrizNoArquivo(FILE *file, char matriz[TAM][TAM], char mat[TAM][TAM], int qntdPalavras, int tamColuna, int tamLinha);
-
+void *search
 int main ( void )
 {
    static const char filename[] = "file.txt";
@@ -16,8 +16,8 @@ int main ( void )
    FILE *file = fopen ( filename, "r" );
    FILE *fileout = fopen (filenameout, "w");
    int tamColuna, tamLinha, qntdPalavras;
-   char Mat[TAM][TAM], MatMatches[TAM][TAM];
-   int i;
+   char Mat[TAM][TAM], MatMatches[TAM][TAM], MatResposta[TAM][TAM];
+   int i, j;
 
 	if( file == NULL ){
 		fprintf(stdout, "Impossivel abrir arquivo.\n");
@@ -26,10 +26,53 @@ int main ( void )
 	else{
 		makeMatriz(file, &Mat, MatMatches, &tamColuna, &tamLinha, &qntdPalavras);
 		//printaMatriz(&Mat, tamLinha, tamColuna);
+		for( i = 0 ; i < tamaLinha ; i++ ){
+			for( j = 0 ; j < tamColuna ; j++ ){
+				
 		putMatrizNoArquivo(fileout, Mat, MatMatches, qntdPalavras, tamColuna, tamLinha);
 		fclose(file);
 	}
 	return 0;
+}
+
+void searchNaMatriz(){
+	for( i = 0 ; i < qntdPalavras ; i++ ){
+		for( k = 0 ; k < tamLinha ; k++ ){
+			for( j = 0 ; j < tamColuna ; j++){
+				if( Mat[k][j] == MatMatches[i] )
+				{
+					// Procura na direita // 
+					if ( tamColuna > (strlen(MatMatches[i])+j) ){ 
+						for( l = 0 ; MatMatches[i][l] == Mat[j+l][k]; l++) {
+							if(MatMatches[i][l+1]=='\0') 
+								printf("Matched: %s em %d %d\n",MatMatches[i], j, k);
+						}
+					}
+					// Procura na esquerda //
+					if (0 <= ( j - strlen(MatMatches[i]) ) ) { 							
+						for( l = 0 ; MatMatches[i][l] == Mat[j-l][k] ; l++) {
+							if(MatMatches[i][l+1]=='\0') 
+								printf("Matched: %s em %d %d\n",MatMatches[i], j, k);
+						}
+					}
+					// Procura na Coluna baixo - cima//
+					if ( tamLinha > (strlen(MatMatches[i]) + k) ) { 
+						for( l = 0 ; MatMatches[i][l] == Mat[j][k+l]; l++) {
+							if(MatMatches[i][l+1]=='\0')
+								printf("Matched: %s em %d %d\n",MatMatches[i], j, k);
+						}
+					}
+					// Procura na Coluna cima - baixo //
+					if (0 <= (k - strlen(MatMatches[i])) ) { 
+						for( l = 0; MatMatches[i][l] == Mat[j][k-l]; l++) {
+							if(MatMatches[i][l+1]=='\0')
+								printf("Matched: %s em %d %d\n",MatMatches[i], j, k);
+								}
+					}
+				}
+		}
+	}
+}
 }
 
 void putMatrizNoArquivo(FILE *file, char matriz[TAM][TAM], char mat[TAM][TAM], int qntdPalavras, int tamColuna, int tamLinha){
@@ -44,7 +87,6 @@ void putMatrizNoArquivo(FILE *file, char matriz[TAM][TAM], char mat[TAM][TAM], i
 	for( i = 0 ; i <= qntdPalavras ; i++ )
 		fprintf(file, "%s\n", mat[i]);
 }
-
 
 void printaMatriz( char (*matriz)[TAM][TAM], int tamLinha, int tamColuna ){
 	int i, j;
@@ -91,3 +133,4 @@ int getTamColuna(FILE *file){
 	fscanf(file, "%d", &tamColuna);
 	return tamColuna;
 }
+
